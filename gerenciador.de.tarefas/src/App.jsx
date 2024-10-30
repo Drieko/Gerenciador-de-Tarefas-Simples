09e86c71770cc069d40a2a47b49df96f3e06c799
Drieko <driekodev@gmail.com>
2024-10-30 20:38:48 -0300

    B

--- gerenciador.de.tarefas/src/App.jsx
+++ gerenciador.de.tarefas/src/App.jsx
@@ -3,10 +3,9 @@ import TodoApp from './components/ToDoApp'
 
 function App() {
 
-const teste = import.meta.env.VITE_TESTE
+ 
   return (
     <>
-    <h1>poggers</h1>
       <TodoApp />
     </>
   )
